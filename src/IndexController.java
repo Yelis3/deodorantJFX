8fa088e95f5320c87b4e35c4e2c72110ce95733c
Yeliana Andrea Torres Medina <yatorresm@unal.edu.co>
2018-06-01 04:17:21 -0500

    prev and next buttons

--- src/IndexController.java
+++ src/IndexController.java
@@ -5,11 +5,34 @@ import javafx.scene.control.TextArea;
 import javax.swing.*;
 
 public class IndexController {
-    public Label detection;
+    public Label detectionInfo;
+    public Label refactorInfo;
+    public Label smellTitle;
     public TextArea detectEditor;
+    public TextArea refactorEditor;
+    public static final String[] smells = {"unusedParameters", "duplicatedConditionalFragments", "longParameterList"};
+    public int smellIndex = 0;
+    public static final String[] smellNames = {"Unused Parameter in Methon", "Duplicated Conditional Fragments", "Long Parameter List"};
 
     public void detect(ActionEvent actionEvent) {
         String text = detectEditor.getText().replaceAll("\n", System.getProperty("line.separator"));
-        detection.setText(text);
+        detectionInfo.setText("detector: " + text);
+    }
+
+    public void refactor(ActionEvent actionEvent) {
+        String text = detectEditor.getText().replaceAll("\n", System.getProperty("line.separator"));
+        refactorInfo.setText("refactor: " + text);
+    }
+
+    public void prev(ActionEvent actionEvent) {
+        if(smellIndex > 0)
+            smellIndex --;
+        smellTitle.setText(smellNames[smellIndex]);
+    }
+
+    public void next(ActionEvent actionEvent) {
+        if(smellIndex < smells.length-1)
+            smellIndex ++;
+        smellTitle.setText(smellNames[smellIndex]);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
