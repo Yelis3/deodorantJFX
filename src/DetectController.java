9772720c736843d63043e43b5a3f03eefe8ae304
Yeliana Andrea Torres Medina <yatorresm@unal.edu.co>
2018-06-01 02:57:28 -0500

    alkams

---
+++ src/DetectController.java
@@ -0,0 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class DetectController {
+    public Label detection;
+
+    public void detect(ActionEvent actionEvent) {
+
+        detection.setText("I am detecting!");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("index.fxml"));
+        primaryStage.setTitle("Python Code Deodorant");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
